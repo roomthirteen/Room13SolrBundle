f703cb825170dc7541c22f02470ff09a2a99a3ae
Benjamin Seibert <benjamin.seibert@gmx.de>
2012-11-07 10:31:27 +0100

    removed unused clearIndex method from solr service

--- Solr/SolrService.php
+++ Solr/SolrService.php
@@ -5,9 +5,4 @@ namespace Room13\SolrBundle\Solr;
 class SolrService extends \Apache_Solr_Service
 {
 
-    public function clearIndex()
-    {
-        $this->delete('<delete><query>*:*</query></delete>');
-        $this->commit(true);
-    }
 }
